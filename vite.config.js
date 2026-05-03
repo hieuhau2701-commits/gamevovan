9ed820d1527bed8f24dbec5924ae9f90f7ec36b2
hieuhau2701-commits <hieuhau2701@gmail.com>
2026-05-03 18:07:18 +0700

    Delete vite.config.js

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  // RẤT QUAN TRỌNG: Thêm dòng base này và điền đúng tên Repository của bạn
-  base: '/gamevovan/', 
-})
